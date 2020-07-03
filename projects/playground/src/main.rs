cea87c8db4616d794930e750614507982345da10
7hoenix <jpholzmann@gmail.com>
2020-07-03 16:59:08 -0500

    Apparently better options than match, generally.

--- projects/playground/src/main.rs
+++ projects/playground/src/main.rs
@@ -2,17 +2,13 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt");
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("problem creating {:?}", e),
-            },
-            other_error => {
-                panic!("problem opening {:?}", other_error)
-            }
-        },
-    };
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("problem creating {:?}", error);
+            })
+        } else {
+            panic!("problem opening {:?}", error)
+        }
+    });
 }
