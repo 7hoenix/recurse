5f066fc8fa745eda237299a6b318fca675e16cbd
7hoenix <jpholzmann@gmail.com>
2020-07-03 16:54:58 -0500

    Nested pattern matching.

--- projects/playground/src/main.rs
+++ projects/playground/src/main.rs
@@ -1,8 +1,18 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    let mut v = vec![1];
-    v.push(2);
-    for i in &mut v {
-        *i += 40;
-    }
-    v[88];
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("problem creating {:?}", e),
+            },
+            other_error => {
+                panic!("problem opening {:?}", other_error)
+            }
+        },
+    };
 }
