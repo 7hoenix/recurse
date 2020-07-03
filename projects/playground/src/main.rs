129d65e7bf81b2691b68ea9b48987df087de11a5
7hoenix <jpholzmann@gmail.com>
2020-07-03 17:03:00 -0500

    Expect and unwrap also options.

--- projects/playground/src/main.rs
+++ projects/playground/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
+    let f = File::open("hello.txt").expect("Failed to open");
     let f = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| {
