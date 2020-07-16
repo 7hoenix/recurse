269cc0eaa99340b3f08e7fd935631a502b4eec4e
7hoenix <jpholzmann@gmail.com>
2020-07-16 15:02:25 -0500

    Incrementor

--- mmpe/lib/mmpe_web/live/page_live.ex
+++ mmpe/lib/mmpe_web/live/page_live.ex
@@ -4,12 +4,21 @@ defmodule MMPEWeb.PageLive do
   @impl true
   def render(assigns) do
     ~L"""
-    <p>Hello world</p>
+    <a href="#" phx-click="inc_counter">Increment</a>
+    <p>Counter: <%= @counter %></p>
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
+    socket = assign(socket, :counter, 0)
     {:ok, socket}
   end
+
+  def handle_event("inc_counter", _session, socket) do
+    {:noreply, socket
+      |> assign(:counter, socket.assigns.counter + 1)
+    }
+  end
+
 end
