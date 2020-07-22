f8c92212f5a6b2bea3d51533de288110c8eae35e
7hoenix <jpholzmann@gmail.com>
2020-07-22 15:36:30 -0500

    Builder test 2.

--- proc-macro-workshop/builder/src/lib.rs
+++ proc-macro-workshop/builder/src/lib.rs
@@ -2,11 +2,34 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
-// use quote::{quote};
+use quote::{quote};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ast = parse_macro_input!(input as DeriveInput);
+    let ast = parse_macro_input!(input as DeriveInput);
+    let module_ident = &ast.ident;
+    let with_builder = format!("{}Builder", module_ident);
+    let as_ident = syn::Ident::new(&with_builder, module_ident.span());
 
-    proc_macro::TokenStream::from(proc_macro::TokenStream::new())
+    let expanded = quote! {
+        pub struct #as_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #module_ident {
+            pub fn builder() -> #as_ident {
+                #as_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
 }
