bd65795052b94a5407b54c9730f4d887ff416459
7hoenix <jpholzmann@gmail.com>
2020-07-27 17:02:17 -0500

    Parse out arguments dynamically.

--- proc-macro-workshop/builder/src/lib.rs
+++ proc-macro-workshop/builder/src/lib.rs
@@ -1,57 +1,129 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{parse_macro_input, DeriveInput, Attribute, Data, DataStruct, Fields, FieldsNamed, Field, Type, PathSegment, Path};
+use syn::punctuated::{Punctuated, Pair};
+use syn::token::{Colon2};
 use quote::{quote};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
+    // eprintln!("****************************************");
+    // eprintln!("AST: {:#?}", ast);
+    // eprintln!("****************************************");
+    let noned_attrs: proc_macro2::TokenStream = retrieve_attrs_typed_or_untyped(&ast, None);
+    let typed_attrs: proc_macro2::TokenStream = retrieve_attrs_typed_or_untyped(&ast, Some("doesntmatt"));
+
     let module_ident = &ast.ident;
     let with_builder = format!("{}Builder", module_ident);
-    let as_ident = syn::Ident::new(&with_builder, module_ident.span());
-    let attrs: proc_macro2::TokenStream = ast.attrs.into_iter().map(|cake| { quote! { #cake: None, }}).collect();
-    // for attr in &ast.attrs {
-    //     attrs.append(quote! { #attr });
-    // };
-
+    let module_name_as_ident = syn::Ident::new(&with_builder, module_ident.span());
 
     let expanded = quote! {
-        pub struct #as_ident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+        pub struct #module_name_as_ident {
+            #typed_attrs
         }
 
         impl #module_ident {
-            // fn executable (&mut self, executable: String) -> &mut Self {
-            //     self.executable = Some(executable);
-            //     self
-            // }
-
-            // fn args (&mut self, args: Vec<String>) -> &mut Self {
-            //     self.args = Some(args);
-            //     self
-            // }
-
-            // fn env (&mut self, env: Vec<String>) -> &mut Self {
-            //     self.env = Some(env);
-            //     self
-            // }
-
-            // fn current_dir (&mut self, current_dir: String) -> &mut Self {
-            //     self.current_dir = Some(current_dir);
-            //     self
-            // }
-
-            pub fn builder() -> #as_ident {
-                #as_ident {
-                    #attrs
+            pub fn builder() -> #module_name_as_ident {
+                #module_name_as_ident {
+                    #noned_attrs
                 }
             }
         }
     };
 
     proc_macro::TokenStream::from(expanded)
+}
+
+
+fn retrieve_attrs_typed_or_untyped(ast: &DeriveInput, flag: Option<&str>) -> proc_macro2::TokenStream {
+    match &ast.data {
+        Data::Struct(data_struct) => {
+            match &data_struct.fields {
+                Fields::Named(attrs) => {
+                    let vals: proc_macro2::TokenStream = attrs.named.pairs().map(|puncuated| {
+                        match puncuated {
+                            Pair::Punctuated(field, _) => {
+                                let field_ident = extract_ident(field);
+                                let field_type = extract_type(field);
+
+                                match flag {
+                                    None => {
+                                        let expanded = quote! {
+                                           #field_ident: None,
+                                        };
+                                        proc_macro2::TokenStream::from(expanded)
+                                    },
+                                    Some(_) => {
+                                        let expanded = quote! {
+                                           #field_ident: Option<#field_type>,
+                                        };
+                                        proc_macro2::TokenStream::from(expanded)
+                                    }
+                                }
+
+                            },
+                            Pair::End(_) => {
+                                let expanded = quote! {
+                                };
+                                let cake: proc_macro2::TokenStream = proc_macro2::TokenStream::from(expanded);
+                                cake
+                            }
+                        }
+                    }).collect::<proc_macro2::TokenStream>();
+                    vals
+                },
+                // Fields::Named
+                _ => {
+                    panic!("unhandled");
+                }
+            }
+        },
+        // Data::Struct
+        _ => {
+            panic!("unhandled");
+        }
+    }
+}
+
+fn extract_ident(field: &Field) -> proc_macro2::TokenStream {
+    match &field.ident {
+        Some(attr_ident) => {
+            let expanded = quote! {
+                #attr_ident
+            };
+            let cake: proc_macro2::TokenStream = proc_macro2::TokenStream::from(expanded);
+            cake
+        },
+        None => {
+            let expanded = quote! {
+            };
+            let cake: proc_macro2::TokenStream = proc_macro2::TokenStream::from(expanded);
+            cake
+        }
+    }
+}
+
+fn extract_type(field: &Field) -> proc_macro2::TokenStream {
+    match &field.ty {
+        Type::Path(type_path) => {
+            let maybe_ident = type_path.path.get_ident();
+            match maybe_ident {
+                None => {
+                   panic!("unhandled");
+                },
+                Some(id) => {
+                    let expanded = quote! {
+                        #id
+                    };
+                    proc_macro2::TokenStream::from(expanded)
+                }
+            }
+        },
+        _ => {
+           panic!("unhandled");
+        }
+    }
 }
--- proc-macro-workshop/builder/tests/03-call-setters.rs
+++ proc-macro-workshop/builder/tests/03-call-setters.rs
@@ -16,14 +16,14 @@ use derive_builder::Builder;
 pub struct Command {
     executable: String,
     args: Vec<String>,
-    env: Vec<String>,
+    // env: Vec<String>,
     current_dir: String,
 }
 
 fn main() {
     let mut builder = Command::builder();
-    builder.executable("cargo".to_owned());
-    builder.args(vec!["build".to_owned(), "--release".to_owned()]);
-    builder.env(vec![]);
-    builder.current_dir("..".to_owned());
+    // builder.executable("cargo".to_owned());
+    // builder.args(vec!["build".to_owned(), "--release".to_owned()]);
+    // builder.env(vec![]);
+    // builder.current_dir("..".to_owned());
 }
